6f8cde283adffee1d5730e5b31edb2d234676a35
goutamsamal9 <gsamal101@gmail.com>
2024-04-26 16:03:01 +0530

    Used chakra

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,30 @@
 import React from "react";
+import {
+  ChakraProvider,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 
 const MyComponent = () => {
-  return <div>Hello from My Component!</div>;
+  return (
+    <ChakraProvider>
+      <Box p={5} shadow="md" borderWidth="1px">
+        <FormControl>
+          <FormLabel htmlFor="email">Name</FormLabel>
+          <Input id="email" type="name" />
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input id="email" type="email" />
+
+          <Button mt={4} colorScheme="teal" type="submit">
+            Submit
+          </Button>
+        </FormControl>
+      </Box>
+    </ChakraProvider>
+  );
 };
 
 export default MyComponent;
